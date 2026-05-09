024f0657e42e16cf39179e365ef9a81cfad0e9b7
oscarhuanca1-source <oscarhuanca1@gmail.com>
2026-05-09 12:26:26 -0500

    Excercise 3 Solution

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -7,7 +7,7 @@ class Program
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
 
         Random randomGenerator = new Random();
-        int number = randomGenerator.Next(1, 20);
+        int number = randomGenerator.Next(1, 21);
         
         // Console.WriteLine(number);
         // I use this to find out the generated number
