7333c1a892025ef0ee1de7e70467ce35baf6ee79
oscarhuanca1-source <oscarhuanca1@gmail.com>
2026-05-11 14:20:41 -0500

    2nd commit

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,16 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        // Console.WriteLine("Hello World! This is the Exercise3 Project.");
 
         Random randomGenerator = new Random();
-        int number = randomGenerator.Next(1, 21);
+        int number = randomGenerator.Next(1, 101);
         
-        // Console.WriteLine(number);
         // I use this to find out the generated number
-
-        Console.WriteLine("What is the magic number? ");
-        Console.Write("What is your guess? ");
+        // I delete the redundant question "What is your guess? "
+        Console.WriteLine("Welcome to the guess the number game, the magic number is within the range of 1 to 100");
+        Console.Write("What is the magic number? ");
         string numberGuess = Console.ReadLine();
         int numberInt = int.Parse(numberGuess);
         
